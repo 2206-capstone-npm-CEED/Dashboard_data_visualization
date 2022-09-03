6d33a48a0f2330a40be3944d79ae82f29fea83f1
Emre BASBAYDAR <EmreMacbook@Emres-MacBook-Pro.local>
2022-09-03 16:29:07 -0400

    d3 logo fixed

--- client/components/Aboutus.js
+++ client/components/Aboutus.js
@@ -5,28 +5,29 @@ import React from 'react'
 
   render() {
     return (
-        
-     
-        <div>
+
+
+        <div id='aboutUs'>
             <h2>
         <div>Chris - Likes walks along the beach</div>
-            
+
         <div>Ethan - Ate some granola</div>
         <div>Emre - He very good at changing baby diapers</div>
         <div>Dat - Just made up this about us section</div>
-            
+
             </h2>
-            
+
             <div id="logo">
 	<div id="logocontainer">
 		<img className="topics" id="css3" src="//inf3cti0n95.github.io/img/css3.svg"/>
 		<img className="topics" id="html5" src="//inf3cti0n95.github.io/img/html5.svg"/>
 		<img className="topics" id="js" src="//inf3cti0n95.github.io/img/js.svg"/>
     <img className="topics" id="D3" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS7EGo-Lt2Kw58PqIegd7MlYUTr5G7kE3vdJA&usqp=CAU"/>
+
 	</div>
 </div>
         </div>
-    
+
     )
   }
 }
